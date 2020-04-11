5d4c674071010af6560bdd3d0f144c8655a9bca2
Denis <dev.denism@yandex.ru>
2020-04-11 18:01:53 +1000

    completed + fix images/top menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -7,12 +7,30 @@ $(document).ready(function(){
         menuButton.children('.menu-burger__view').toggleClass('menu-burger__view--open');
         if ( menuButton.children('.menu-burger__view').hasClass('menu-burger__view--open')){
             menu.slideDown(300, ()=>{
-                menu.css({'display': 'flex'})
+                menu.addClass('main-menu--open');
+                menu.css({'display': ''})
             });
         }
         else{
-            menu.slideUp();
+             menu.slideUp(300, ()=>{
+                 menu.removeClass('main-menu--open');
+                 menu.css({'display': ''})
+             });
         }
     })
+
+ /*   let mediaQueryMin630px = window.matchMedia("(max-width: 631px)");
+    mediaQueryMin630px.addEventListener('change',()=>{
+        if (mediaQueryMin630px.matches){
+            menu.css('display', 'flex');
+        }
+    })
+
+        let mediaQueryMax630px = window.matchMedia("(max-width: 630px)");
+        mediaQueryMax630px.addEventListener('change',()=>{
+            if (mediaQueryMax630px.matches){
+                menu.css('display', 'none');
+            }
+        })*/
 
 })
